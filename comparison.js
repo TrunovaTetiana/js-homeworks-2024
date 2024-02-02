0a24858a7ff49dfbf87a7f4932469c21379ad0db
tanya2810 <145008963+TrunovaTetiana@users.noreply.github.com>
2024-02-02 17:55:22 +0100

    Add files via upload
    HW02

---
+++ comparison.js
@@ -0,0 +1,18 @@
+let userNumber=prompt('Tytle number');
+
+if (userNumber==5){
+    alert ('You guessed!!!');
+}
+else if
+    (userNumber<10){
+    alert('This is very small number!')
+}
+
+    
+else {
+    userNumber>10
+    alert('This is very large number!')
+
+}
+
+
