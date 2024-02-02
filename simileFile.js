790e8bc76203372b0f2bec979c2fa169ace9ef62
tanya2810 <145008963+TrunovaTetiana@users.noreply.github.com>
2024-02-02 18:02:45 +0100

    HW02

---
+++ simileFile.js
@@ -0,0 +1,15 @@
+let firstValue=prompt('Type number 1');
+let secondValue=prompt("Type number 2");
+
+if (firstValue===secondValue){
+    alert ('good');
+}
+else if
+   ('firstValue>secondValue');{
+    alert (`is wrong: ${firstValue>secondValue}`);
+}
+ if('firstValue<secondValue');{
+    alert(`correct, ${firstValue<secondValue}`);
+}
+ 
+
