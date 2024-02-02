4fb6653c47a5bbec1c761801a73e86030ff02c7c
tanya2810 <145008963+TrunovaTetiana@users.noreply.github.com>
2024-02-02 17:59:55 +0100

    HW02

---
+++ number.js
@@ -0,0 +1,8 @@
+let numValue=prompt('Write the number');
+
+if (numValue==1||numValue==100||numValue>1||numValue<100){
+    alert('number in rahge');
+} 
+else if(numValue<1||numValue>100);{
+    alert('number behinde the range');
+ } 
