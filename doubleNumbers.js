9bf9f42bf6d6f288120a6fb7edb9a5b5284fa6ea
tanya2810 <145008963+TrunovaTetiana@users.noreply.github.com>
2024-02-02 17:58:57 +0100

    HW02

---
+++ doubleNumbers.js
@@ -0,0 +1,13 @@
+let firstValue=prompt('Type number1');
+let secondValue=prompt('Type numer2');
+let rezult=(+firstValue%+secondValue);
+
+if (rezult==0){
+    alert ('double number');
+}
+else
+    (rezult>(+firstValue%+secondValue)); {
+     alert('unpaired number');
+    }
+
+
