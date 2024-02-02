c450a7be97278f14d52ce9a555e0351c879a3724
tanya2810 <145008963+TrunovaTetiana@users.noreply.github.com>
2024-02-02 12:19:35 +0100

    Add files via upload
    HW01

---
+++ main.js
@@ -0,0 +1,12 @@
+let userName='Jon';
+alert(userName);
+let userScore=25;
+alert(userScore);
+
+let priceProdukt = prompt('price');
+let quantityProdukt = prompt('quantity');
+
+let total = prompt(+priceProdukt * +quantityProdukt);
+alert (total);
+let percentTotal = prompt((+priceProdukt * +quantityProdukt)*5/100);
+alert (percentTotal);
