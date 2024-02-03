95c4a7159041908cad4c0aa207b02c96c89afc66
tanya2810 <145008963+TrunovaTetiana@users.noreply.github.com>
2024-02-03 19:04:06 +0100

    HW02

--- points.js
+++ points.js
@@ -3,16 +3,17 @@ let pointsNumber=prompt('Write your rating.');
 
 if 
 (pointsNumber==8||pointsNumber==9||pointsNumber==10){
-    alert('Great!!!')
+    alert('Great!!!');
 }
 else if
-    (pointsNumber==5|pointsNumber==6|pointsNumber==7)
-    alert('Good!!')
-    
- else 
-    (pointsNumber==3||pointsNumber==4)
-    alert('satisfactorily')
-      
- if
-(pointsNumber==1||pointsNumber==2)
-    alert('Not satisfactory')
+    (pointsNumber==5||pointsNumber==6||pointsNumber==7){
+    alert('Good!!');
+    }   
+ else if
+    (pointsNumber==3||pointsNumber==4){
+    alert('satisfactorily');
+ }    
+ else{
+pointsNumber==1||pointsNumber==2
+    alert('Not satisfactory');
+ }
